547364dae7725d969b227f157e761791ef7b9c23
1404927255 <115683268+1404927255@users.noreply.github.com>
2022-10-13 12:46:56 +0800

    Add files via upload

---
+++ 作业if.cpp
@@ -0,0 +1,17 @@
+﻿#include<stdio.h>
+int main(void)
+{
+	float w, h,t;
+	printf("请输入您的体重和身高:> ");
+	scanf_s("%f %f", &w, &h);
+	t = w / (2 * h);
+	if (t < 18)
+		printf("低体重\n");
+	else if (18 <= t < 25)
+		printf("正常体重\n");
+	else if (25 <= t < 27)
+		printf("超重体重\n");
+	else if (27 <= t)
+		printf("肥胖\n");
+	return 0;
+}
