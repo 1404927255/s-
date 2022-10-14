ba0cf107ebc58ed11324a86bf63c83c5a8a63abc
1404927255 <115683268+1404927255@users.noreply.github.com>
2022-10-14 23:00:50 +0800

    Add files via upload

---
+++ while.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int line = 0;
+	printf("孤单的世界，你也是一个人在前进吗？\n");
+	while (line <=1314)
+	{
+		printf("我爱你，无畏人海的勇气:%d\n", line);
+		line++;
+	}
+	if (line = 1315)
+		printf("希望以后牵手的人都是你\n");
+	return 0;
+}
